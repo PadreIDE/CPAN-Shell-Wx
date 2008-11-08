f579cefdd85078026687d6d5402f7c2c7772c2d0
szabgab <szabgab@gmail.com>
2008-11-08 08:25:40 +0000

    CPAN::Shell::Wx new method
    git-svn-id: http://svn.perlide.org/padre/projects/CPAN-Shell-Wx@807 bc93166f-e056-4eb9-8c59-0a0840647e1b

--- lib/CPAN/Shell/Wx.pm
+++ lib/CPAN/Shell/Wx.pm
@@ -5,5 +5,21 @@ use warnings;
 
 our $VERSION = '0.01';
 
+use CPAN;
+use CPAN::Shell::Wx::App;
+
+sub new {
+	my ($class) = @_;
+
+	$CPAN::Frontend ||= "CPAN::Shell::Wx::Front";
+
+	return bless {}, $class;
+}
+
+sub run {
+	my ($self) = @_;
+	CPAN::Shell::Wx::App->new;
+}
+
 
 1;
